4fc4f9cfc8d0b394b5759011a292ee85a656e9d6
I Kadek Yuda Budipratama Giri <13516115@std.stei.itb.ac.id>
2019-10-30 05:51:50 +0700

    Revision on parser and another data

--- crawler.py
+++ crawler.py
@@ -1,16 +1,17 @@
-import datetime
+from datetime import datetime
 import requests
 import csv
 import time
 
 from bs4 import BeautifulSoup as soup
-from pprint import pprint
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-base_url = "https://www.olx.co.id"
-page_base_url = "https://www.olx.co.id/mobil-bekas_c198?page="
+base_url = "https://www.olx.co.id/mobil-bekas_c198"
+car_url_prefix = "https://www.olx.co.id"
+MUAT_LAINNYA_XPATH = "/html/body/div/div/main/div/section/div/div/div[5]/div[2]/div[1]/div[3]/button"
 
 def crawlCarData(car_url):
-    print('[ INFO ] ' + 'Crawling from ' + car_url)
     response = requests.get(car_url)
     page = soup(response.text, 'html.parser')
     value_tag = page.find_all(class_="_2vNpt")
@@ -22,42 +23,47 @@ def crawlCarData(car_url):
         value_list.append(int(page.find(class_='_2xKfz').contents[0].replace('.','').replace('Rp', '')))
     return value_list
 
-def crawlPage(page_url):
-    print('[ INFO ] ' + 'Crawling from ' + page_url)
-    response = requests.get(page_url)
-    page = soup(response.text, 'html.parser')
-    car_tags = page.find_all(class_='EIR5N')
-    car_data = []
-    for cars in car_tags:
-        car_link = cars.find('a', href=True)
-        car_url = base_url + car_link['href']
-        value_list = crawlCarData(car_url)
-        car_data.append(value_list)
-        time.sleep(2)
-    return car_data
-
-def crawlWebsite(page = 1):
-    print('[ INFO ] ' + 'Crawling from ' + base_url)
-    data = []
-    for i in range(page):
-        page_url = page_base_url + str(i+1)
-        results = crawlPage(page_url)
-        data += results
-        time.sleep(2)
-    print('[ INFO ] ' + 'Total data crawled: ' + str(len(data)))
-    return data
-
 def writeCSV(data, filename = 'test.csv'):
-    with open(filename, 'w', newline='') as csvfile:
+    with open(filename, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
         for row in data:
             writer.writerow(row)
     csvfile.close()
 
-def crawlCSV():
-    data = crawlWebsite(page=1)
-    now = datetime.datetime.now()
-    fileName = str(now.year) + str(now.month) + str(now.day) + "-" + str(now.hour) + "_" + str(now.minute) + "_" + str(now.second) + "_carlist.csv"
-    writeCSV(data, filename=fileName)
+def crawlWebsite():
+    print('[ INFO ] ' + 'Crawling from ' + base_url)
+    options = Options()
+    options.headless = True
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(5)
+    driver.get(base_url)
+    more_button = driver.find_elements_by_xpath(MUAT_LAINNYA_XPATH)
+    btn_counter = 0
+    while (len(more_button) > 0):
+        more_button[0].click()
+        btn_counter += 1
+        print('[ INFO ] Clicked MUAT LAINNYA! Number of clicks: ' +  str(btn_counter))
+        time.sleep(2)
+        more_button = driver.find_elements_by_xpath(MUAT_LAINNYA_XPATH)
+    print('[ INFO ] No more MUAT LAINNYA button. Oh boy time to parse!')
+    page = soup(driver.page_source, 'html.parser')
+    car_tags = page.find_all(class_='EIR5N')
+    car_data = []
+    print('[ INFO ] Total link: ' + str(len(car_tags)))
+    for cars in car_tags:
+        car_link = cars.find('a', href=True)
+        car_url = car_url_prefix + car_link['href']
+        value_list = crawlCarData(car_url)
+        car_data.append(value_list)
+        percentage = str(int((len(car_data) / len(car_tags))*100)).rjust(3)
+        print('[ ' + percentage + '% ] ' + 'Crawled from ' + car_url)
+        time.sleep(2)
+    driver.close()
+    print('[ INFO ] Crawling finished. Total data crawled: ' + str(len(car_data)))
+    return car_data
 
-crawlCSV()
+data = crawlWebsite()
+now = datetime.today()
+filename = str(now.year) + str(now.month) + str(now.day) + "-" + str(int(now.timestamp())) + '_carlist.csv'
+writeCSV(data,filename=filename)
+print('[ INFO ] Crawled data saved to ' + filename)
