2c947437d0119e623c71639faf1a4b5b7be3d39c
I Kadek Yuda Budipratama Giri <13516115@std.stei.itb.ac.id>
2019-10-30 09:21:04 +0700

    Fixed crawler

--- crawler.py
+++ crawler.py
@@ -9,23 +9,36 @@ from selenium.webdriver.chrome.options import Options
 
 base_url = "https://www.olx.co.id/mobil-bekas_c198"
 car_url_prefix = "https://www.olx.co.id"
+HEADERS = ['Merek', 'Varian', 'Model', 'Tahun', 'Jarak tempuh', 'Tipe bahan bakar', 'Warna', 'Transmisi', 'Sistem Penggerak', 'Tipe bodi', 'Kapasitas mesin', 'Harga']
 MUAT_LAINNYA_XPATH = "/html/body/div/div/main/div/section/div/div/div[5]/div[2]/div[1]/div[3]/button"
 
 def crawlCarData(car_url):
     response = requests.get(car_url)
     page = soup(response.text, 'html.parser')
-    value_tag = page.find_all(class_="_2vNpt")
-    value_list = []
-    for value in value_tag:
-        value_list.append(value.contents[0])
+    header_tag = page.find_all(class_="_25oXN")
+    value_tag  = page.find_all(class_="_2vNpt")
+    value_dict = {}
+    for i in HEADERS:
+        value_dict[i] = '?'
+    for header, value in zip(header_tag, value_tag):
+        if (header.contents[0] in HEADERS):
+            value_dict[header.contents[0]] = value.contents[0]
     price = page.find(class_='_2xKfz')
     if price:
-        value_list.append(int(page.find(class_='_2xKfz').contents[0].replace('.','').replace('Rp', '')))
-    return value_list
+        value_dict['Harga'] = int(page.find(class_='_2xKfz').contents[0].replace('.','').replace('Rp', ''))
+    return value_dict
 
 def writeCSV(data, filename = 'test.csv'):
     with open(filename, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
+        for row in data:
+            writer.writerow(row)
+    csvfile.close()
+
+def writeDictToCSV(data, filename = 'test.csv', fieldnames = HEADERS):
+    with open(filename, 'a', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
         for row in data:
             writer.writerow(row)
     csvfile.close()
@@ -53,10 +66,11 @@ def crawlWebsite():
     for cars in car_tags:
         car_link = cars.find('a', href=True)
         car_url = car_url_prefix + car_link['href']
-        value_list = crawlCarData(car_url)
-        car_data.append(value_list)
-        percentage = str(int((len(car_data) / len(car_tags))*100)).rjust(3)
-        print('[ ' + percentage + '% ] ' + 'Crawled from ' + car_url)
+        value_dict = crawlCarData(car_url)
+        if (bool(value_dict)):
+            car_data.append(value_dict)
+            percentage = str(int((len(car_data) / len(car_tags))*100)).rjust(3)
+            print('[ ' + percentage + '% ] ' + 'Crawled from ' + car_url)
         time.sleep(2)
     driver.close()
     print('[ INFO ] Crawling finished. Total data crawled: ' + str(len(car_data)))
@@ -65,5 +79,5 @@ def crawlWebsite():
 data = crawlWebsite()
 now = datetime.today()
 filename = str(now.year) + str(now.month) + str(now.day) + "-" + str(int(now.timestamp())) + '_carlist.csv'
-writeCSV(data,filename=filename)
+writeDictToCSV(data, filename=filename, fieldnames=HEADERS)
 print('[ INFO ] Crawled data saved to ' + filename)
